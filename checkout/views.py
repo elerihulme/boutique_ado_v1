2275f89becf24b669f262f20e965cb8fa7e3dfdf
Eleri Hulme <eleri.hulme@live.co.uk>
2025-05-04 17:31:43 +0100

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51RL54WJqyArePMNy8Vf1mJ9q5AHIes9SWroh27LxqKuhnKvFtKelEeUBw2vCCrt1EEEvaWEXOhnv0HNIulB05m5B00ooHqoo0n',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
